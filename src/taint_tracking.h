0fd7d195f32208d6741d21a64a48147b091f5687
wisdom <yangchengchang2008@163.com>
2026-04-26 11:18:57 +0800

    feat: 完成 Taint Tracking 补丁的现代 V8 伪代码适配与移植

--- src/taint_tracking.h
+++ src/taint_tracking.h
@@ -7,7 +7,7 @@
 
 #include "include/v8.h"
 #include "src/base/utils/random-number-generator.h"
-#include "src/objects/objects.h"
+#include "src/objects.h"
 #include "src/parsing/token.h"
 
 #include <fstream>
@@ -21,8 +21,8 @@ namespace internal {
 class FunctionLiteral;
 class Parser;
 class ParseInfo;
-}  // namespace internal
-}  // namespace v8
+};
+};
 
 namespace tainttracking {
 
@@ -164,17 +164,6 @@ enum BranchType {
   CONDITIONAL
 };
 
-// Taint runtime metadata for implicit and explicit call sites that need
-// symbolic stack-frame tracking.
-enum FrameType {
-  RUNTIME_CALL,
-  TO_STRING_CONVERT_PLUS_LEFT,
-  TO_STRING_CONVERT_PLUS_RIGHT,
-  UNKNOWN_CAPI,
-  UNKNOWN_CAPI_NEW,
-  UNKNOWN_EXTERNAL
-};
-
 typedef uint32_t TaintFlag;
 const TaintFlag kTaintFlagUntainted = 0;
 
@@ -196,7 +185,7 @@ struct TaintInstanceInfo {
 
 class TaintListener {
 public:
-  virtual ~TaintListener() {}
+  virtual ~TaintListener() {};
   virtual void OnTaintedCompilation(const TaintInstanceInfo& info,
                                     v8::internal::Isolate* isolate) = 0;
 };
@@ -328,7 +317,7 @@ void JSSetTaintBuffer(
     v8::internal::Handle<v8::internal::String> str,
     v8::internal::Handle<v8::internal::JSArrayBuffer> data);
 
-v8::internal::Handle<v8::internal::JSArrayBuffer>
+MUST_USE_RESULT v8::internal::Handle<v8::internal::JSArrayBuffer>
 JSGetTaintStatus(v8::internal::Handle<v8::internal::String> str,
                  v8::internal::Isolate* isolate);
 void JSTaintLog(v8::internal::Handle<v8::internal::String> str,
@@ -339,7 +328,7 @@ v8::internal::Handle<v8::internal::Object> JSCheckTaintMaybeLog(
     v8::internal::Handle<v8::internal::Object> tag,
     int symbolic_data);
 
-v8::internal::Handle<v8::internal::HeapObject>
+MUST_USE_RESULT v8::internal::Handle<v8::internal::HeapObject>
 JSTaintConstants(v8::internal::Isolate* isolate);
 
 
